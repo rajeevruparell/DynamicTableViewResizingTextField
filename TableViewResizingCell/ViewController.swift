862d964d53a6353418206b6d8f66e6eeada2c18d
Rajeev <rajeev@fastmail.com>
2017-07-25 14:17:33 -0400

    Practice Project Working

--- TableViewResizingCell/ViewController.swift
+++ TableViewResizingCell/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController, UITextViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    tableView.estimatedRowHeight = 70
+    tableView.rowHeight = UITableViewAutomaticDimension
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//MARK: - TableView Methods
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MyCell
+        cell.textView.delegate = self
+        return cell
+    }
+    
+    
+//MARK: - TextView
+    func textViewDidChange(_ textView: UITextView) {
+        
+        //Animation and Offset code added to fix stuttering
+        let currentOffset = tableView.contentOffset
+        UIView.setAnimationsEnabled(false)
+        tableView.beginUpdates()
+        tableView.endUpdates()
+        UIView.setAnimationsEnabled(true)
+        tableView.setContentOffset(currentOffset, animated: false)
+    }
+    
 }
+
+
+
+class MyCell: UITableViewCell {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+}
+
 
